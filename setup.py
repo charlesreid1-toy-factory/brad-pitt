7cf401c14a5f7a622749d1af87cc2d8ce22f2fa0
Chaz Reid <charlesreid1.ancestry@gmail.com>
2022-09-04 08:49:36 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ with open("requirements-dev.txt") as f:
 setup(
     name="brad-pitt",
     version=__version__,
+    version="0.1.0",
     description="a super simple python package demonstrating best practices for versioning",
     author="Chaz Reid",
     author_email="charlesreid1.ancestry@ancestry.com",
@@ -16,10 +17,15 @@ setup(
     packages=["brad_pitt"],
     package_dir={"brad_pitt": "src"},
     package_data={
-        "gollyx_maps": [
+        "brad_pitt": [
             "data/*.json",
             "data/*.txt",
         ]
+    },
+    entry_points={
+        'console_scripts': [
+            'bp=brad_pitt.cli:main',
+        ],
     },
     license="MIT",
     install_requires=all_deps,
