7956278e92c6836cdf1620e55cbef91e492f1c9c
Chaz Reid <charlesreid1.ancestry@gmail.com>
2022-09-04 10:35:08 -0700

    extra exception stuff

--- src/error.py
+++ src/error.py
@@ -1,3 +1,17 @@
+import random
+import json
+
 class PittFall(Exception):
     """This class is just here for the clever wordplay"""
-    pass
+
+    def __init__(self, *args, **kwargs):
+        print("-"*40)
+        print(self.random_fact())
+        print("-"*40)
+        super(*args, **kwargs)
+
+    def random_fact(self):
+        with open('data/brad_pitt_facts.json', 'r') as f:
+            d = json.load(f)
+        facts = d['list_of_facts']
+        return random.choice(facts)
