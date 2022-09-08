e5f32a1f63f4e812f3a55824159cf12d0793b876
Charles Reid <charlesreid1@gmail.com>
2022-09-08 08:42:30 -0700

    fix typo

--- common.mk
+++ common.mk
@@ -1,4 +1,4 @@
 ifeq ($(shell echo ${BRAD_PITT_HOME}),)
-$(error Environment variable BRAD_PITT_HOME not defined. Please run "source environment" in the gollyx-maps repo root directory before running make commands)
+$(error Environment variable BRAD_PITT_HOME not defined. Please run "source environment" in the repo root directory before running make commands)
 endif
 
