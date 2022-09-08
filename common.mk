283017a84f6b29c60c893e31714cdad39db38b5c
Chaz Reid <charlesreid1.ancestry@gmail.com>
2022-09-08 14:52:11 -0700

    check for bump2version

--- common.mk
+++ common.mk
@@ -1,3 +1,9 @@
+SHELL=/bin/bash
+
+ifeq ($(shell which bump2version),)
+$(error Please install bump2version or activate your virtual environment)
+endif
+
 ifeq ($(shell echo ${BRAD_PITT_HOME}),)
 $(error Environment variable BRAD_PITT_HOME not defined. Please run "source environment" in the repo root directory before running make commands)
 endif
